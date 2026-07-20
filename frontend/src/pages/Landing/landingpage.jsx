546b0a8e952682b9e683c1e4630687bd5877a8af
Michael Emmanuel <emmanuelmichael2006@gmail.com>
2026-07-20 14:46:34 +0100

    Add stats section to landing page

--- frontend/src/pages/Landing/landingpage.jsx
+++ frontend/src/pages/Landing/landingpage.jsx
@@ -2,12 +2,14 @@ import Hero from "../../components/Landing/hero";
 import Navbar from "../../components/Landing/Navbar";
 import Features from "../../components/Landing/features";
 import featuresCard from "../../components/Landing/featureCard";
+import Stats from "../../components/Landing/stats";
 
 function landingPage() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Stats/>
       <Features />
       <featureCard/>
     </>
